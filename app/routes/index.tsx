bb7cd27426bb1b4500fc5964237ce3a1fce171e5
markingvar <office@jensendesignshop.com>
2023-01-01 20:30:07 -0700

    test alternate configuration

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,20 +2,27 @@ import {useState, useRef, useLayoutEffect} from 'react';
 import gsap from 'gsap';
 
 export default function Index() {
-const [tl, setTl] = useState();
-
-const onEnter = ({ currentTarget }) => {
-    gsap.to(currentTarget, { backgroundColor: "#e77614", scale: 1.2 });
-  };
-  
-  const onLeave = ({ currentTarget }) => {
-    gsap.to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
-  };
      
   return (
     <div className="app flex-row">
-      <div className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
-        Hover Me
-      </div>
+    {typeof window !== 'undefined' && <SuperDuper />}
     </div>  );}
 
+
+function SuperDuper() {
+    const [tl, setTl] = useState();
+
+    const onEnter = ({ currentTarget }) => {
+        gsap.to(currentTarget, { backgroundColor: "#e77614", scale: 1.2 });
+    };
+
+    const onLeave = ({ currentTarget }) => {
+        gsap.to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
+    };
+  return (
+          <div className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
+          Hover Me
+          </div>
+
+  )
+    }
