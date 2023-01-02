705c6abfc5ae142429fbe85678dd95020e3ab2e1
markingvar <office@jensendesignshop.com>
2023-01-01 19:56:44 -0700

    test Math.random

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,21 +1,22 @@
 import {useState, useRef, useLayoutEffect} from 'react';
-import gsap from 'gsap';
+// import gsap from 'gsap';
 
 export default function Index() {
-const [tl, setTl] = useState();
+// const [tl, setTl] = useState();
 
-const onEnter = ({ currentTarget }) => {
-    gsap.to(currentTarget, { backgroundColor: "#e77614", scale: 1.2 });
-  };
+// const onEnter = ({ currentTarget }) => {
+//    gsap.to(currentTarget, { backgroundColor: "#e77614", scale: 1.2 });
+//  };
   
-  const onLeave = ({ currentTarget }) => {
-    gsap.to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
-  };
-     
+//  const onLeave = ({ currentTarget }) => {
+//    gsap.to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
+//  };
+  console.log(Math.random());
   return (
     <div className="app flex-row">
-      <div className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
+    Hello dawgs
+      /* <div className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
         Hover Me
-      </div>
+      </div>*/
     </div>  );}
 
