d591f24b31305231c3155bb763c93cceec533896
markingvar <office@jensendesignshop.com>
2023-01-02 08:49:37 -0700

    try just exposing gsap.to

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -10,15 +10,15 @@ export default function Index() {
 
 
 function SuperDuper() {
-    let gsap = useGsap();
+    let {to} = useGsap();
     const [tl, setTl] = useState();
 
     const onEnter = ({ currentTarget }) => {
-        gsap.to(currentTarget, { backgroundColor: "#e77614", scale: 1.2 });
+        to(currentTarget, { backgroundColor: "#e77614", scale: 1.2 });
     };
 
     const onLeave = ({ currentTarget }) => {
-        gsap.to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
+        to(currentTarget, { backgroundColor: "#28a92b", scale: 1 });
     };
   return (
           <div className="box" onMouseEnter={onEnter} onMouseLeave={onLeave}>
--- app/utils/gsap.ts
+++ app/utils/gsap.ts
@@ -1,5 +1,7 @@
 import gsap from 'gsap';
 
 export function useGsap() {
-  return gsap;
+  return {
+    to: gsap.to,
+  };
 }
