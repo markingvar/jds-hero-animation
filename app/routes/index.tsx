a1233cbbc647a6d36fd586f5685824571ea3f094
markingvar <office@jensendesignshop.com>
2023-01-02 09:06:14 -0700

    Just tell me who your daddy is

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,11 @@
 import {useState, useRef, useLayoutEffect} from 'react';
 
 export default function Index() {
-    console.log({global, self});
+    console.log({global: global, self: self});
+
+    console.log("Hi I'm Daisy! This is global: ", global);
+
+    console.log("Hi, I'm Peach, this is self: ", self);
      
   return (
     <div className="app flex-row">
